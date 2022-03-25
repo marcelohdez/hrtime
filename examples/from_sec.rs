93c68a0f17b385ad685db33ffecb7b97256a7ce2
Marcelo Hernandez <76508651+marcelohdez@users.noreply.github.com>
2022-03-24 20:01:54 -0400

    removed assert equals in examples
    These should be in the tests insetad

--- examples/from_sec.rs
+++ examples/from_sec.rs
@@ -6,10 +6,4 @@ fn main() {
     sec = 28309;
     println!("{sec} is {},", hrtime::from_sec(sec));
     println!("and {sec} with padding is {}!", hrtime::from_sec_padded(sec));
-
-    // All pass:
-    assert_eq!("2:57", hrtime::from_sec(177));
-    assert_eq!("1:01:02", hrtime::from_sec(3662));
-    assert_eq!("03:02:57", hrtime::from_sec_padded(10977));
-    assert_eq!("00:00:00", hrtime::from_sec_padded(0));
 }
--- examples/to_sec.rs
+++ examples/to_sec.rs
@@ -2,10 +2,4 @@ fn main() {
     let time = "11:23:08";
     let sec = hrtime::to_sec(time);
     println!("{time} is {sec} seconds!");
-
-    // All pass:
-    assert_eq!(225, hrtime::to_sec("3:45"));
-    assert_eq!(4320, hrtime::to_sec("1:12:00"));
-    assert_eq!(18, hrtime::to_sec("18"));
-    assert_eq!(19211, hrtime::to_sec("320:11"));
 }
